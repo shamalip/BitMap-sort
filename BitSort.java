2009a35aef72d0178b7d5f6541536b943345fd66
Shamali Patwa <43223058+shamalip@users.noreply.github.com>
2018-10-13 10:08:59 -0700

    Error handling for duplicate records.

--- BitSort.java
+++ BitSort.java
@@ -10,21 +10,28 @@ import java.util.stream.IntStream;
  */
 public class BitSort {
 
-	
 	/**
-	 * @param arr - The input array to be sorted
-	 * @param limit - The max value in range
-	 * @return - The sorted array.
+	 * @param arr The input array to be sorted
+	 * @param limit the max value in range
+	 * @return The sorted array.
+	 * The complexity of this method is O(n)
 	 */
 	public static int[] sort(int[] arr,int limit) {
 		BitSet bitMap = new BitSet(limit+1);
-		IntStream.of(arr).forEach(num -> bitMap.set(num));
+		IntStream.of(arr).forEach(num -> setBit(bitMap,num));
 		int j = 0;
 		for (int i = bitMap.nextSetBit(0); i >= 0; i = bitMap.nextSetBit(i+1)) {
 			if (i != -1) { arr[j] = i; j++; }
-			if (i == Integer.MAX_VALUE) { break; }			
+			if (i == Integer.MAX_VALUE) { break; }	
 		}
 		return arr;
+	}
+
+	private static void setBit(BitSet bitMap, int num) {
+		if(bitMap.get(num)) { /** If the bit is already set **/
+			throw new IllegalArgumentException("The input has duplicate records.");
+		}
+		bitMap.set(num);
 	}
 
 }
